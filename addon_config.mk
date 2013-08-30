2dea8e0c8d3373319fa425677e8d553de1c99a58
Christopher Baker <me@christopherbaker.net>
2013-08-30 02:15:34 -0500

    Add config.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = Capture video streams from IP Cameras.
+	ADDON_DESCRIPTION = Capture video streams from IP Cameras.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "ipvideo" "video" "security camera" "ipcamera"
+	ADDON_URL = http://github.com/bakercp/ofxIpVideoGrabber
